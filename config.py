a714208f976a77651f0d762930bd7f99c83f5993
Srinath88847 <soldier.91088@gmail.com>
2024-12-09 10:35:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7723342093:AAF3zzCsPWzmA8irXGbrjm079HqAqJ9IJS0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("soldiersri")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1424497176")
+
+
+
+
+  
